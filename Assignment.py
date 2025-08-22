148002b4457d61aa28203283e634425176863092
funkycoder36 <ifti80@yahoo.com>
2025-08-22 21:17:19 +0500

    Assignment

---
+++ Assignment.py
@@ -0,0 +1,9 @@
+r = int(input("Enter the total number of rows: "))
+print("Mirrored right triangle star pattern")
+for i in range(1,r+1):
+    for j in range(1,r+1):
+        if(j <= r -i):
+            print(' ', end = ' ')
+        else:
+            print('*', end = ' ')
+    print()
